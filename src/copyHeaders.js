1ab776e0c2f310daf35abd985795ebe27a4db0c1
Rushs321 <fyggh566@tutamail.com>
2024-09-05 23:13:41 +0530

    Update copyHeaders.js

--- src/copyHeaders.js
+++ src/copyHeaders.js
@@ -1,10 +1,1 @@
-"use strict";
-export function copyHeaders(response, reply) {
-  for (const [key, value] of Object.entries(response.headers)) {
-    try {
-      reply.header(key, value);
-    } catch (e) {
-      console.log(e.message);
-    }
-  }
-}
+"use strict";export function copyHeaders(response,reply){for(const[key,value]of Object.entries(response.headers)){try{reply.header(key,value)}catch(e){console.log(e.message)}}}
